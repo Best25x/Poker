0afc5e598a824c8313715bfad8bedcff5553e83b
Best25x <xBest25x@gmail.com>
2024-05-20 20:56:44 -0700

    working on the actual game class main function

--- main.py
+++ main.py
@@ -2,15 +2,6 @@
 import numpy as np
 
 
-#   parameters
-player_count = 2
-start_wealth = 100
-small_blind = 5
-big_blind = 10
-minimum_bid = 1
-
-players = {}
-
 
 #   card & deck generation
 values = list(range(2, 15))
@@ -39,12 +30,14 @@ class Card:
 
 class Deck:
 
-    def __init__(self, deck):
-        self.deck = deck
+    def __init__(self):
+        self.deck = []
+        self.generate_deck(values, suits)
+        self.shuffle_deck()
 
-    def generate_deck(self, values, suits):
-        for value in values:
-            for suit in suits:
+    def generate_deck(self, vals, sui):
+        for value in vals:
+            for suit in sui:
                 if value in face_cards:
                     real_value = face_cards[value]
                 else:
@@ -54,10 +47,14 @@ class Deck:
     def shuffle_deck(self):
         np.random.shuffle(self.deck)
 
-    def draw_card(self):
-        drawn_card = self.deck[0]
-        self.deck.pop(0)
+    def draw_card(self, card=None):
+        if card is type(Card): #for manually setting drawn cards
+            drawn_card = card
+        else:
+            drawn_card = self.deck[0]
+            self.deck.pop(0)
         return drawn_card
+
 
     def print_deck(self):
         for card in self.deck:
@@ -82,7 +79,6 @@ class Evaluator:
 
     @staticmethod
     def flush_high_card(cards): #jank as hell but it works i think :)
-        dist = Evaluator.hand_dist(cards)
         for card in cards:
             if cards[0].suit != card.suit:
                 return None
@@ -102,7 +98,7 @@ class Evaluator:
         for i in range(2,15):
             if i == exclude:
                 continue
-            if (dist[i] == num_of_a_kind):
+            if dist[i] == num_of_a_kind:
                 return i
         return None
 
@@ -143,108 +139,133 @@ class Evaluator:
 
 
 #players
-class AI_Player:
-
-    def __init__(self, hand, wealth, in_pool, is_folded):
-        self.hand = hand
-        self.wealth = wealth
-        self.in_pool = in_pool
-        self.is_folded = is_folded
-
-    def generate_hand(self):
-        for i in range(2):
-            self.hand.append(game_deck.draw_card())
-
-    def print_hand(self):
-        for card in self.hand:
-            card.print_card()
-
-    def make_move(self, previous_bet):
-        move = 3 #MAGIC HAPPENS
-
-        if move == 1:
-            #MORE MAGIC HAPPENS
-            bet = 0
-            self.wealth -= bet
-            self.in_pool += bet
-        elif move == 2:
-            amount = previous_bet - self.in_pool
-            self.wealth -= amount
-            self.in_pool += amount
-        elif move == 3:
-            self.is_folded = True
 
 
 class Player:
 
-    def __init__(self, hand, wealth, in_pool, is_folded):
-        self.hand = hand
+    def __init__(self, wealth, name):
         self.wealth = wealth
-        self.in_pool = in_pool
-        self.is_folded = is_folded
+        self.name = name
 
-    def generate_hand(self):
-        for i in range(2):
-            self.hand.append(game_deck.draw_card())
+        self.hand = []
+        self.in_pot = 0
+        self.is_folded = False
+
 
     def print_hand(self):
         for card in self.hand:
             card.print_card()
 
     def make_move(self, previous_bet):
-        while True:
-            move = input("Make a move: (1=bet, 2=check/call, 3=fold): ")
-            if type(move) is int and move > 0 and move <= 3:
-                break
+        um = True
+        while um:
+            try:
+                move = int(input("Make a move: (1=bet, 2=check/call, 3=fold): "))
+            except ValueError:
+                print("That's not an integer. ", end="")
+            else:
+                um = False
+
+
         if move == 1:
             uhh = True
             while uhh:
-                bet = input("How much do you want to bet/raise? ")
                 try:
-                    int(bet)
+                    bet = int(input("How much do you want to bet/raise? "))
                 except ValueError:
-                    pass
+                    print("That's not an integer. ", end="")
                 else:
                     uhh = False
             self.wealth -= bet
-            self.in_pool += bet
+            self.in_pot += bet
+
         elif move == 2:
-            amount = previous_bet - self.in_pool
+            amount = previous_bet - self.in_pot
             self.wealth -= amount
-            self.in_pool += amount
+            self.in_pot += amount
+
         elif move == 3:
             self.is_folded = True
+
+class AI_Player:
+
+    def __init__(self, wealth, name):
+        self.wealth = wealth
+        self.name = name
+
+        self.hand = []
+        self.in_pot = 0
+        self.is_folded = False
+
 
 
 # GAME
 class Game:
+    small_blind = 5
+    big_blind = 10
 
-    def __init__(self, deck, pot, flop, burn, round):
-        self.deck = deck
-        self.pot = pot
-        self.flop = flop
-        self.burn = burn
-        self.round = round
+    def __init__(self, player_count, start_wealth):
+        self.player_count = player_count
+        self.start_wealth = start_wealth
+
+        self.deck = Deck()
+        self.pot = 0
+        self.flop = []
+        self.players = {}
+        self.round = 2
 
     def main(self):
-        self.deck = Deck([])
-        self.deck.generate_deck(values,suits)
-        self.deck.shuffle_deck()
+        self.make_players()
+
+        dealer_id = ((self.round-1) % self.player_count) + 1 #NO 0th PLAYER
+        small_blind_id = (dealer_id % self.player_count) + 1 #NO 0th PLAYER
+        big_blind_id = (small_blind_id % self.player_count) + 1 #NO 0th PLAYER
+        new_line()
+        print(f"{self.players[dealer_id].name} is dealing.\n{self.players[small_blind_id].name} has to pay small blind ({Game.small_blind}).\n{self.players[big_blind_id].name} has to pay big blind ({Game.big_blind}).")
+
+        self.get_blind()
 
 
 
-#   game variables
+        # if self.players[small_blind_id].wealth >= Game.small_blind:
+        #     self.players[small_blind_id] -= Game.small_blind
+        #     self.pot += Game.small_blind
+        # else:
+        #     ... #small has to fold and shifts over one
+        #
+        # if self.players[big_blind_id].wealth >= Game.big_blind:
+        #     self.players[big_blind_id].wealth -= Game.big_blind
+        #     self.pot += Game.big_blind
+        # else:
+        #     ... #big has to fold and shifts over one
 
-for count in range(player_count-1):
-    players[count] = Player([],start_wealth, 0, False)
-players[player_count-1] = AI_Player([], start_wealth, 0, False)
+        #request small and big blind
+
+    def make_players(self):
+        for i in range(1,self.player_count+1): #FIRST PLAYER IS PLAYER 1 NOT 0
+            name = input(f"What is Player {i}'s name? (Type 'AI' for AI player): ")
+            if name == "AI":
+                self.players[i] = AI_Player()
+            else:
+                self.players[i] = Player(self.start_wealth, name)
+
+    def get_blind(self):
+        ...
+
+def new_line():
+    print("--------------------------------------")
+
+
+game = Game(5, 100) #player_count has to be >= 3
+game.main()
+
 
 
 
 #   TEST CASES FOR PROGRAMMING AGH
-hands = {
-    1:[Card(14,'C'),Card(7,'D'),Card(2,'S'),Card(7,'S'),Card(7,'H')]
-}
-
-for id, hand in hands.items():
-    print(f"{id} : {Evaluator.eval_hand(hand)}")
+# hands = {
+#     1:[Card(14,'C'),Card(7,'D'),Card(2,'S'),Card(7,'S'),Card(7,'H')]
+# }
+#
+# for id, hand in hands.items():
+#     print(f"{id} : {Evaluator.eval_hand(hand)}")
